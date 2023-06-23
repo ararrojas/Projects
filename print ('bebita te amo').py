ad376efaec50243f8c347536288e8c6f4d844ce3
Ara Rojas <araceli.rojas123@gmail.com>
2023-06-23 15:31:53 +0200

    changes

---
+++ print ('bebita te amo').py
@@ -0,0 +1,7 @@
+print ('bebita te amo')
+import random
+list=['chocolate conocut','hazzlenut','cookies and cream','black biscuit','biscuit']
+res=random.choice(list)
+if res=='cookies and cream':
+    print('mala leche')
+print(res)
